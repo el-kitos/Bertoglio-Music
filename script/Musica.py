327493f4a4bcf9066a3e6ce3eb5a6fb9199c7c5a
marki <mbertoglio@alumno.huergo.edu.ar>
2026-04-08 09:55:17 -0300

    hola

---
+++ script/Musica.py
@@ -0,0 +1,25 @@
+class Botones:
+    def __init__(self, ventana, pos_x, pos_y, imagen, action):
+        self.ventana = ventana
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.imagen = imagen
+        self.action = action
+    
+    def play():
+        pass
+    
+    def pause():
+        pass
+
+    def stop():
+        pass
+
+    def next():
+        pass
+
+    def previous():
+        pass
+
+    def volume():
+        pass
---
+++ script/main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import messagebox
+
+ventana = tk.Tk()
+ventana.title("Reproductor de Musica")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ventana.mainloop()
