f080f2db02d68d1576f171a2f7197af0531d61a8
marki <mbertoglio@alumno.huergo.edu.ar>
2026-04-08 10:15:36 -0300

    uptade de imagenes

--- script/main.py
+++ script/main.py
@@ -3,6 +3,11 @@ from tkinter import messagebox
 
 ventana = tk.Tk()
 ventana.title("Reproductor de Musica")
+ventana.geometry("1920x1080")
+
+nombre = tk.Entry()
+nombre = nombre.grid(row=100, column=100)
+
 
 
 
